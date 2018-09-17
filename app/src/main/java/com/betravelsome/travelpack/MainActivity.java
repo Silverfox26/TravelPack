c6df464ea98fa4a4b6e445ec464e7f736ecbc375
Daniel Penz <daniel.penz@gmx.de>
2018-09-17 10:38:06 +0200

    feat: implement add trip FAB

--- app/src/main/java/com/betravelsome/travelpack/MainActivity.java
+++ app/src/main/java/com/betravelsome/travelpack/MainActivity.java
@@ -1,5 +1,6 @@
 package com.betravelsome.travelpack;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,8 +23,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // Start EditTrip Activity
+                Intent addTripIntend = new Intent(MainActivity.this, EditTripActivity.class);
+                startActivity(addTripIntend);
             }
         });
     }
